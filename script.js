5aa03ca1fff1c888e58d867d4be0f6f9f0e15921
vikrantsrgc <145971065+vikrantsrgc@users.noreply.github.com>
2025-04-07 10:13:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function firstWord(str) {
 
   return str.trim().split(' ')[0];
 }
-
+  
 // Do not change the code below
 
-// const s = prompt("Enter String:");
+const s = prompt("Enter String:");
 alert(firstWord(s));
